f4988625aa9d0571634aff624687068386dd9bdd
wpeterashworth <wingsaseagles12@gmail.com>
2024-12-05 15:04:40 -0800

    Moved work to a different repo becuase I didn't fully read the instructions for the final

--- final/scripts/hamburger.js
+++
@@ -1,7 +0,0 @@
-let menuButton = document.querySelector("#menu");
-let navList = document.querySelector("nav ul")
-menuButton.addEventListener("click", () => {
-    menuButton.classList.toggle("open")
-    navList.classList.toggle("open")
-})
-
